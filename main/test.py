20104e4d7fed6c46deda60518fd3b35c3958356d
Whale <xh1870211055@gmail.com>
2023-11-27 16:02:18 +0800

    Update test.py

--- main/test.py
+++ main/test.py
@@ -5,13 +5,13 @@ import subprocess
 
     
 def run_CountersAndBenchmark():
-    proc_event=subprocess.Popen("./preset_native_test",shell=True)
+    proc_event=subprocess.Popen("./MainProgram",shell=True)
     for i in range(6):
         os.system("python testbenchmark.py")
-        proc_benchmark=subprocess.Popen("../mcftp test_benchmark.xml",shell=True)
-        pid2=os.getpid()
+        proc_benchmark=subprocess.Popen("./mcftp test_benchmark.xml",shell=True)
         bm_outs,bm_errs=proc_benchmark.communicate()
-    ev_outs,ev_errs=proc_event.communicate()
+    event_outs,event_errs=proc_event.communicate()
+    
     
 
 if __name__=="__main__":
